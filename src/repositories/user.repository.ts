9e51bd38a1b5b7cfc353302c1d2bbe9f886dc205
mysteryman123456 <nschldhl61@gmail.com>
2026-02-06 20:13:00 +0545

    Changes in user repo and add file upload controller

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,15 +1,41 @@
-import { UpdateUserProfileData } from "../dtos/user.dto";
+import { GeneralUserDataType, UpdateUserProfileData } from "../dtos/user.dto";
 import { IUser, UserModel } from "../models/user.model";
 
+type SearchedUsers = Pick<
+  GeneralUserDataType,
+  "email" | "username" | "profile_image"
+> & {
+  _id: string;
+};
+
 export class UserRepository {
+  //
+  async getSearchedUser(user: string): Promise<SearchedUsers[]> {
+    const searched_user = await UserModel.findOne(
+      {
+        email: { $regex: user, $options: "i" },
+      },
+      { email: 1, username: 1, profile_image: 1, _id: 1 }
+    );
+    if (!searched_user) return [];
+    return [
+      {
+        email: searched_user.email,
+        username: searched_user.username,
+        profile_image: searched_user.profile_image,
+        _id: searched_user._id.toString(),
+      },
+    ];
+  }
+  //
   async getUserByEmail(email: string): Promise<IUser | null> {
     return UserModel.findOne({ email });
   }
-
+  //
   async getUserById(id: string): Promise<IUser | null> {
     return UserModel.findById(id);
   }
-
+  //
   async updateProfile(
     id: string,
     data: UpdateUserProfileData
