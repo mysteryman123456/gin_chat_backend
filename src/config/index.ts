085cecdd00c8acef0d0055cd316abac5242a844a
mysteryman123456 <nschldhl61@gmail.com>
2026-02-06 20:09:06 +0545

    Changes in confog added pagination limit

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,3 +7,5 @@ export const PORT = 8080;
 export const MONGO_URI = process.env.MONGO_URI!;
 
 export const JWT_SECRET = process.env.JWT_SECRET || "this_is_a_default_secret";
+
+export const DEFAULT_PAGINATION_LIMIT = 3;
