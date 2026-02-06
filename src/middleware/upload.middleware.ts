fb6e7245460a4f1610df32dde0bc9f6b60d95067
mysteryman123456 <nschldhl61@gmail.com>
2026-02-06 20:11:06 +0545

    Added admin panel features

--- src/middleware/upload.middleware.ts
+++ src/middleware/upload.middleware.ts
@@ -1,20 +1,38 @@
-import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
+import multer from "multer";
 import cloudinary from "../config/cloudinary";
+import { HttpError } from "../utils/http_error";
+
+export type FileType = "AUDIO" | "VIDEO" | "FILE" | "IMAGE" | "TEXT";
+
+const fileFormats: Record<FileType, string[]> = {
+  IMAGE: ["jpg", "png", "jpeg", "webp"],
+  VIDEO: ["mp4", "mov", "avi", "mkv"],
+  AUDIO: ["mp3", "wav", "aac", "mpeg"],
+  FILE: ["pdf", "doc", "docx", "txt", "csv", "plain", "zip"],
+  TEXT: ["txt"],
+};
 
 const storage = new CloudinaryStorage({
   cloudinary,
-  params: async () => ({
-    folder: "uploads",
-    allowed_formats: ["jpg", "png", "jpeg", "webp"],
-  }),
-});
+  params: async (_, file) => {
+    const ext = file.mimetype.split("/").pop()?.toLowerCase();
+    if (!ext) {
+      throw new HttpError("Invalid file type", 400);
+    }
 
-const upload = multer({
-  storage,
-  limits: {
-    fileSize: 3 * 1024 * 1024,
+    const isValid = Object.values(fileFormats).some((formats) =>
+      formats.includes(ext)
+    );
+
+    if (!isValid) {
+      throw new HttpError("Invalid file type", 400);
+    }
+
+    return {
+      resource_type: file.mimetype.startsWith("video") ? "video" : "auto",
+    };
   },
 });
 
-export default upload;
+export const upload = multer({ storage });
